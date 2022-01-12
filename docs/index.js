11f31610f95e71301c2b2ef9d2ec07d3ea06e3a6
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-01-12 17:57:06 +0300

    socketio

--- docs/index.js
+++ docs/index.js
@@ -2,7 +2,7 @@
 let myVideo = document.getElementById("video");
 var videoListener;
 
-var ws = new WebSocket("wss://usher-api.herokuapp.com/ws");
+var ws = new WebSocket("ws://localhost:8001/ws");
 
 ws.onopen = function () {
     console.log("Connected to server");
