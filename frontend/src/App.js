321cfcba36c3a3b79065c35e4ef1d9d700a92fda
batuhanbaybas <b.baybas@gmail.com>
2022-01-24 00:51:26 +0300

    have some bug but volume controller play/stop and fullscren control done

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoPlayer from './Components/VideoPlayer';
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className='container'>
+      <VideoPlayer />
+    </div>
+  );
+};
+
+export default App;
