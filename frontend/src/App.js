e338203ea1272e22bdebcc563cde568d8253c07c
batuhanbaybas <b.baybas@gmail.com>
2022-02-14 00:39:40 +0300

    some update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react';
-import VideoPlayer from './Components/VideoPlayer';
-import "./App.css"
+import VideoPlayer from 'react-video-js-player';
 
-const App = () => {
-  const [video, setVideo] = useState(false)
 
+function App() {
 
   return (
-    <div className='container'>
-
-      <VideoPlayer />
-
-
+    <div className='h-screen bg-slate-800 flex flex-col items-center'>
+      <div className='text-2xl my-14 w-full text-white text-center'>Usher Media player</div>
+      <div className='flex'>
+        <VideoPlayer
+          controls={true}
+          src={"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"}
+          width="720"
+          height="420"
+        />
+        <div className='text-white mx-5'>
+          odadakiler
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
