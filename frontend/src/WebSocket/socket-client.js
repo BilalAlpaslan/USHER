09dde8ab2d1d2cf439251e4fbdf099fb85d27984
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-03-02 15:02:49 +0300

    websocker init

--- frontend/src/WebSocket/socket-client.js
+++ frontend/src/WebSocket/socket-client.js
@@ -1,9 +1,94 @@
-const ws = new WebSocket("ws://164.92.250.16:8000/ws");
+var ws
+var myVideo
+var videoListener;
+var second;
 
 
-export const wsConnect = () => {
+function playPause() {
+    if (myVideo.paused) {
+        myVideo.play();
+        ws.send(JSON.stringify({ "type": "play" }));
+    } else {
+        myVideo.pause();
+        ws.send(JSON.stringify({ "type": "pause" }));
+    }
+}
+
+function makeBig() {
+    myVideo.width += 30;
+}
+
+function makeSmall() {
+    myVideo.width -= 30;
+}
+
+function chVideo(e) {
+    var file = this.files[0]
+    var fileURL = URL.createObjectURL(file)
+    console.log("changed =>", e.target.value);
+    myVideo.src = fileURL;
+}
+
+function goToSecond() {
+    ws.send(JSON.stringify({ "type": "goToSecond", "second": second.value }));
+}
+
+function setSecond(_second) {
+    console.log("setSecond =>", _second);
+    myVideo.currentTime = _second;
+}
+
+function hideNav() {
+    document.getElementById("nav").style.display = "none";
+    document.getElementById("nav-hide").style.display = "block";
+}
+
+function openNav() {
+    document.getElementById("nav").style.display = "block";
+    document.getElementById("nav-hide").style.display = "none";
+}
+
+function back(i) {
+    myVideo.currentTime -= i;
+    ws.send(JSON.stringify({ "type": "goToSecond", "second": myVideo.currentTime }));
+}
+
+function forward(i) {
+    myVideo.currentTime += i;
+    ws.send(JSON.stringify({ "type": "goToSecond", "second": myVideo.currentTime }));
+}
+
+export const wsConnect = (videoRef) => {
+    myVideo = videoRef
+    var client_name = prompt("Enter your name", "Guest");
+    ws = new WebSocket("ws://192.168.1.104:8001/ws");
+
+    var url = new URL(window.location.href);
+    var video_name = url.searchParams.get("v");
+    var isAdmin = url.searchParams.get("admin"); //TODO: admin settings
+    console.log("is admin:", isAdmin);
+
+    setTimeout(function () {
+        if (video_name && video_name.length > 0) {
+            myVideo = document.getElementById("video");
+            myVideo.src = "./video/" + video_name;
+        }
+    }, 1000);
+
     ws.onopen = () => {
         console.log("Connected to server");
-        ws.send("Hello Server");
+    }
+
+    ws.onclose = function () { alert("Connection closed") }
+
+    ws.onmessage = function (event) {
+        var data = JSON.parse(event.data);
+        console.log(data);
+        if (data.data === "play")
+            myVideo.play();
+        else if (data.data === "pause")
+            myVideo.pause();
+        else if (data.data === "goToSecond")
+            setSecond(data.second);
     }
 }
