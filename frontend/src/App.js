230d88bf41a2fe048aef6a442fa36561db6bdbff
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-04-16 19:33:28 +0300

    welcome screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,70 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react'
 import VideoPlayer from 'react-video-js-player';
 import { roomEntity } from './bloc/room';
 import { wsConnect } from './WebSocket/socket-client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 
-function App() {
-
-  var rooms = roomEntity.use()
+const Player = () => {
+  const [isReady, setReady] = useState(false)
+  const [username, setUsername] = useState()
   const videoRef = useRef()
+  var rooms = roomEntity.use()
 
-  useEffect(() => { 
-    wsConnect(videoRef)
-  }, []);
+
+  useEffect(() => {
+    if (isReady) {
+      if (username === null || username === undefined || username === "") setUsername("Guest")
+      wsConnect(videoRef, username)
+    }
+  }, [isReady]);
 
   return (
     <div className='h-screen bg-slate-800 flex flex-col items-center'>
       <div className='text-2xl my-14 w-full text-white text-center'>Usher Media player</div>
-      <div className='flex'>
-        <VideoPlayer
-          ref={videoRef}
-          controls={true}
-          src={"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"}
-          width="720"
-          height="420"
-        />
-        <div className='text-white mx-5'>
-          odadakiler
-          {rooms ? rooms.map((person, index)=> <p key={index}>{person}</p> ) : null}
-        </div>
-      </div>
+      {
+        isReady ?
+          (
+            <div className='flex'>
+              <VideoPlayer
+                ref={videoRef}
+                controls={true}
+                src={"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"}
+                width="720"
+                height="420"
+              />
+              <div className='text-white mx-5'>
+                odadakiler
+                {rooms ? rooms.map((person, index) => <p key={index}>{person}</p>) : null}
+              </div>
+            </div>
+          )
+          : (
+            <div className='flex flex-col items-center'>
+              <input
+                className='w-full my-4' type='text' placeholder='Username'
+                onChange={(e) => setUsername(e.target.value)} />
+              <button className='w-full my-4 bg-blue-500 text-white' onClick={() => setReady(true)}>Connect</button>
+            </div>
+          )
+      }
     </div>
+  )
+}
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Player />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
